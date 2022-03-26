072f766a62b627a519322906cc0a61f6a7199fec
peterallanwalker <38834582+peterallanwalker@users.noreply.github.com>
2022-03-26 20:59:41 +0000

    tested and refactored support for pushing labels

--- connectIO.py
+++ connectIO.py
@@ -1,3 +1,14 @@
 # ConnectIO
-# For testing SWP08/Probel router control
+# For testing SWP08/Probel router control - cross-point switching + label pushing
+# Peter Walker, March 2022
 
+import connection_settings as config
+import connection
+import swp_message
+
+if __name__ == '__main__':
+    heading = "SWPO8 Router Control - Cross-point switching and label pushing"
+    header_width = len(heading) + 8
+    print("\n{}\n -- {} --\n{}".format(header_width * '#', heading, header_width * '-'))
+
+    config = config.get_settings()
---
+++ connection_settings.py
@@ -0,0 +1,139 @@
+# Connection Settings handler
+# Peter Walker 2020.
+# Stripped down from CSCP-MIDI for simple SWP use
+# (removed port option as SWP uses fixed port, and removed all the MIDI port handling)
+
+import json
+
+CONFIG_FILE = "connection_settings.json"
+
+
+# Intended private, for use by this script
+def _yes_or_no(string, enter=False, edit=False):
+    """
+    For parsing cmd line input from user - yes/no prompts
+    if string is 'y' or 'yes', case-insensitive, returns 'y'
+    if string is 'n' or 'no', case-insensitive, returns 'n'
+    else, returns False
+    :param string: typically, user input('y/n?')
+    :param enter: optional, if enter=True, when string is empty (Enter key alone), returns 'y'
+    :param edit: optional, if edit=True, when string is 'e'/'E', return 'n'
+    :return: 'y' for affirmative response/acceptance, 'n' for negative response or edit, False for invalid response
+    """
+    string = string.lower().strip()
+
+    if string in ("y", "yes") or (string == "" and enter):
+        return "y"
+    elif string in ("n", "no") or (string == "e" and edit):
+        return "n"
+    else:
+        return False
+
+
+def _validate_ip_address(address):
+    """
+    Checks if input is a valid IPv4 address
+    :param address: string
+    :return: True if valid IPv4 address, else False
+    """
+    address = address.split(".")
+
+    if len(address) != 4:
+        return False
+
+    for segment in address:
+        try:
+            segment = int(segment)
+        except ValueError:
+            return False
+
+        if segment not in range(255):
+            return False
+
+    return True
+
+
+def _load_settings():
+    """
+    Check if configuration file exists
+    :return: Dict of settings (returns defaults if none saved)
+    """
+    try:
+        with open(CONFIG_FILE, "r") as config:
+            r = json.load(config)
+
+    except FileNotFoundError:
+        print("'{}' file not found. Enter a few details to get started...".format(CONFIG_FILE))
+        r = False
+
+    except json.decoder.JSONDecodeError:
+        print("'{}' file is invalid. Enter a few details to get started...".format(CONFIG_FILE))
+        r = False
+
+    if not r:
+        r = {"Router IP Address": None}
+    return r
+
+
+def _ask_ip_address():
+    """
+    Asks user to input an IP address
+    Checks input is a valid IP address, keeps asking until it is
+    :return: string - user inputted IP address
+    """
+    valid = False
+    while not valid:
+        ip_address = input("Enter mixer's IP address: ")
+        if _validate_ip_address(ip_address):
+            return ip_address
+
+
+def _confirm_settings(config):
+    """
+    Ask user if they want to keep the last used settings or enter new ones
+    :return: Dict of user confirmed settings
+    """
+    use_settings = False
+
+    # Present last used settings and ask to confirm or update
+    while not use_settings:
+        print("Current Connection Settings: ")
+        for heading in config:
+            print("\t", heading, ":", config[heading])
+
+        use_settings = _yes_or_no(input("\nUse these settings? (y/n): "), enter=True)
+
+    if use_settings == "n":
+        # User does not want to keep last used settings, so get their input for new settings
+        config["Router IP Address"] = _ask_ip_address()
+
+    return config
+
+
+# intended public, for use from outside of this script
+def get_settings():
+    """
+    Loads connection settings (just IP address in this version) & gets user to confirm or edit
+    :return: dict of user confirmed settings
+    """
+    r = _load_settings()
+    r = _confirm_settings(r)
+    return r
+
+
+def save_settings(config):
+    with open(CONFIG_FILE, "w") as f:
+        json.dump(config, f, indent=4)
+
+
+if __name__ == '__main__':
+
+    heading = "SWPO8 connection settings"
+    header_width = len(heading) + 8
+    print("\n{}\n -- {} --\n{}".format(header_width * '#', heading, header_width * '-'))
+
+    settings = get_settings()
+    save_settings(settings)
+
+    for setting in settings:
+        print(setting, ":", settings[setting])
