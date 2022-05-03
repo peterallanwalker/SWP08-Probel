25fbe0800599bdd43d2ca534729846a976bb7b66
peteralllanwalker <peter.allan.walker@gmail.com>
2022-05-03 17:37:21 +0100

    fixing cross-point grid nudging

--- connectIO_gui_08.py
+++ connectIO_gui_08.py
@@ -51,6 +51,7 @@
 # - handle router IP changes during runtime
 
 # DISPLAY LOOP COUNT AND FPS
+# option to save/load connection address/name
 
 import sys
 import time
@@ -510,19 +511,24 @@ class MessagingView(QWidget):
 
     def add_received(self, message):
         self.received_message_quantity += 1
-        timestamp = format_timestamp(message[0])
+        message_text = "Received: " + format_timestamp(message[0]) + " - "
+        #timestamp = format_timestamp(message[0])
 
-        message_container = QLabel("RECEIVED at " + timestamp + ":\n" + message[1].__str__())
+        #message_container = QLabel("RECEIVED at " + timestamp + ":\n" + message[1].__str__())
+        message_container = QLabel()
 
         if message[1].command == "ACK" or message[1].command == "NAK":
             # - Apply CSS['ACK'] or CSS['NAK'] style
             message_container.setStyleSheet(CSS[message[1].command])
-
-        # band messages / alternate the colour each row (unless ACK/NAK)
-        elif self.received_message_quantity % 2:
-            message_container.setStyleSheet(CSS['message even'])
+            message_container.setText(message_text + message[1].command + "(" + str(message[1].encoded) + ")")
         else:
-            message_container.setStyleSheet(CSS['message odd'])
+            message_container.setText(message_text + "\n" + message[1].__str__())
+            # band messages / alternate the colour each row (unless ACK/NAK)
+            if self.received_message_quantity % 2:
+                message_container.setStyleSheet(CSS['message even'])
+            else:
+                message_container.setStyleSheet(CSS['message odd'])
+
         self.received_window.addWidget(message_container)
 
     def closeEvent(self, event):
